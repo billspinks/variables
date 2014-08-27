a3ce5f56e9d6e6a25523f66ffb66b5f8c02515ea
Bill Spinks <bill.spinks@gmail.com>
2014-08-27 12:04:18 -0500

    Initial commit

---
+++ variables practice.py
@@ -0,0 +1,11 @@
+__author__ = 'bill'
+# test to see how varx and strx can be put together
+var1 = 3
+var2 = 5
+str1 = "What the Hell"
+str2 = "are we doing?"
+var3 = var1 + var2
+print(var3)
+print str1 +str2
+print str1*4
+print str2*4
